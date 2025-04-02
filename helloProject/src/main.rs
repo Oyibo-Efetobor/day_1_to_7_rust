225118e7bee0e2d0b556671c33ef14998a8301a3
OYIBO Efetobor George <efetobor3x@gmail.com>
2025-04-02 14:31:19 +0100

    main

--- helloProject/src/main.rs
+++ helloProject/src/main.rs
@@ -3,7 +3,22 @@ fn main(){
    let y: u64 = 100;
    println!("Signed Integer: {}", x);
    println!("Unsigned Integer {}", y);
-}
+
 
 //difference i32 can take (32 bits) and i64 (64 bits)
 //range 
+
+//floats (f32,f64) floating point types
+
+    let pi: f64 = 3.14;
+    println!("Value of pi: {}", pi);
+
+// boolean values: true, false
+    let is_snowing: bool = true;
+    println!("Is it snowing ? {}", is_snowing);
+
+// character Type - char //can only be a 
+//single unicode value
+    let letter: char ='x';
+    println!("What do you say? {}", letter);
+}
