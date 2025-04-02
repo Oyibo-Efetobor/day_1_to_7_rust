d74f3fbfde2064bdfd93e48b65e71bcdc9e5fd3d
OYIBO Efetobor George <efetobor3x@gmail.com>
2025-04-02 15:07:46 +0100

    just adding

--- compound_data_types/src/main.rs
+++ compound_data_types/src/main.rs
@@ -3,4 +3,12 @@
 fn main() {
     let numbers: [i32; 5] = [1,2,3,4,5];
     println!("Number Array: {:?}", numbers);
+
+    //str type
+    let fruits: [&str; 3] = ["Apple", "Banana", "Orange"];
+    println!("Fruits Array: {:?}", fruits);
+    println!("Fruits Array 1st element: {}", fruits[0]);
+    println!("Fruits Array 2nd element: {}", fruits[1]);
+    println!("Fruits Array 3rd element: {}", fruits[2]);
+
 }
