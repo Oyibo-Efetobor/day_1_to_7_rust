1ad241b96f2899ec975db2b65961755ceaf350b2
OYIBO Efetobor George <efetobor3x@gmail.com>
2025-04-02 12:42:59 +0100

    hmm

--- helloProject/src/main.rs
+++ helloProject/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, 🦀 from Cargo!");
+fn main(){
+   let x: i32 = -42;
+   let y: u64 = 100;
+   println!("Signed Integer: {}", x);
+   println!("Unsigned Integer {}", y);
 }
+
+//difference i32 can take (32 bits) and i64 (64 bits)
+//range 
