f52fc7e7ed50f32a0993b70c27667b9620fa2d02
OYIBO Efetobor George <efetobor3x@gmail.com>
2025-04-05 16:30:41 +0100

    main

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,7 +2,20 @@ fn main() {
     hello_world();
     tell_height(32);
     human_id("Efetobor", 56, 182.0);
+
+    let _x: i32 ={
+        let price = 5;
+        let qty = 10;
+        price * qty 
+    };
+    println!("Result is {}", _x);
+
+    add(4, 6);
+    println!("Value of y is {}", add(4, 6));
+
+
 }
+
 
 //Hoisting - can call function anywhere in your code
 fn hello_world(){
@@ -20,4 +33,16 @@ fn human_id(name: &str, age: u32, height: f32){
     and my height is {}cm. ", name, age, height);
 }
 
+//functions return values
+fn add(a: i32, b: i32) -> i32{
+    a+b
+}
+
 //Expressions and Statements
+
+//Expression: Anything that returns a value.
+// Statement: Anything that does not return a value.
+
+//Examples of Expression : 5, true & false, add(3,4), if condition 
+// {value 1} else {value 2}
+
